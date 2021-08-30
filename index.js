ad057c87d8862a04a23db8a2767c97097065a044
jddolvin <justindolvin@gmail.com>
2021-08-30 00:49:38 -0400

    cleaned up code

--- index.js
+++ index.js
@@ -2,31 +2,6 @@ const formatter = require("./myModules/formatter");
 const Place = require("./constructors/places");
 const Player = require("./constructors/players");
 
-// Get player name
-const getPlayerName = (player) => {
-  return player.name;
-};
-
-// Get player health text
-const getPlayerHealth = (player) => {
-  return player.name + " has health " + player.health;
-};
-
-// Get player location text
-const getPlayerPlace = (player) => {
-  return player.name + " is in " + player.place;
-};
-
-// Define a function to build a string for the display of player items
-const getPlayerItems = (player) => {
-  let itemsString = "Items:" + formatter.newLine();
-  player.items.forEach((item) => {
-    itemsString += "  - " + item + formatter.newLine();
-  });
-
-  return itemsString;
-};
-
 const library = new Place(
   "The Old Library",
   "You are in a library. Dusty books line the walls."
