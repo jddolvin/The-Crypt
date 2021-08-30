9ddb3f6c2d31d79e09500d350f1775c9feb3f674
jddolvin <justindolvin@gmail.com>
2021-08-29 23:18:30 -0400

    added items handler, and player info display function

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let player1 = {
   name: "Justin",
   health: 50,
   place: "The Dungeon of Doom",
+  items: ["A trusty lamp"],
 };
 
 let player2 = {
@@ -27,22 +28,38 @@ const getPlayerPlace = (player) => {
   return player.name + " is in " + player.place;
 };
 
+// Define a function to build a string for the display of player items
+const getPlayerItems = (player) => {
+  let itemsString = "Items:" + spacer.newLine();
+  player.items.forEach((item) => {
+    itemsString += "  - " + item + spacer.newLine();
+  });
+
+  return itemsString;
+};
+
 const getPlayerInfo = (player, borderCharacter) => {
   // Initialize empty string for player info
-  let playerInfo = "";
+  let info = "";
   let place = getPlayerPlace(player);
   let health = getPlayerHealth(player);
   let longest = Math.max(place.length, health.length) + 4;
 
-  playerInfo = spacer.box(getPlayerName(player), longest, borderCharacter);
-  playerInfo += spacer.wrap(place, longest, borderCharacter);
-  playerInfo +=
-    spacer.newLine() + spacer.wrap(health, longest, borderCharacter);
-  playerInfo += spacer.newLine() + spacer.line(longest, borderCharacter);
-  playerInfo += spacer.newLine();
+  info = spacer.box(getPlayerName(player), longest, borderCharacter);
+  info += spacer.wrap(place, longest, borderCharacter);
+  info += spacer.newLine() + spacer.wrap(health, longest, borderCharacter);
+  info += spacer.newLine() + spacer.line(longest, borderCharacter);
+  info += spacer.newLine();
+  info += " " + getPlayerItems(player);
+  info += spacer.newLine();
+  info += spacer.line(longest, borderCharacter);
+  info += spacer.newLine();
 
-  return playerInfo;
+  return info;
 };
 
-console.log(getPlayerInfo(player1, "="));
-console.log(getPlayerInfo(player2, "="));
+const showPlayerInfo = (player, character) => {
+  console.log(getPlayerInfo(player, character));
+};
+
+showPlayerInfo(player1, "+");
